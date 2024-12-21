9fa5b669a4ab6ad74fbcf9541811489d5a4209f4
MikalDev <33675273+MikalDev@users.noreply.github.com>
2024-12-21 09:13:23 -0800

    feat (editor) support editor draw
    - check for init (and if not init)
    - check for loaded (and if not load)
    - check if created, if not, create

--- Editor/Instance.ts
+++ Editor/Instance.ts
@@ -2,6 +2,9 @@ const SDK = globalThis.SDK;
 
 class RenderaControllerInstance extends SDK.IWorldInstanceBase {
 	private modelId = ''
+	private modelLoaded = false;
+	private modelCreated = false;
+	private model: unknown = null;
 	constructor(sdkType: SDK.ITypeBase, inst: SDK.IWorldInstance) {
 		super(sdkType, inst);
 		console.log('RenderaControllerInstance', sdkType, inst);
@@ -10,8 +13,63 @@ class RenderaControllerInstance extends SDK.IWorldInstanceBase {
 	Release() {
 
 	}
-	
+
+	private IsRenderaInitialized(iRenderer: SDK.Gfx.IWebGLRenderer): boolean {
+		// @ts-ignore
+		const rendera = globalThis.mikalRenderaEditor;
+		if (!rendera) return false;
+		if (!rendera.initialized) {
+			if (!rendera.initializing) {
+				rendera.initializing = true;
+				rendera.initialize(iRenderer);
+			}
+			return false;
+		}
+		rendera.initializing = false;
+		return true;
+	}
+
+	private GetEditorBlob(path: string): Blob | null {
+		const project = this._inst.GetProject()
+		const projectFile = project.GetProjectFileByName(path);
+		if (!projectFile) {
+			console.error('[rendera-controller] project file not found', path);
+			return null;
+		}
+		console.log('[rendera-controller] projectFile', projectFile);
+		const blob = projectFile.GetBlob();
+		console.log('[rendera-controller] blob', blob);
+		return blob;
+	}
+
+	private IsModelLoaded(modelId: string): boolean {
+		if (this.modelLoaded) return true;
+		if (modelId === '') return false;
+		// @ts-ignore
+		const rendera = globalThis.mikalRenderaEditor;
+		if (!rendera) return false;
+		if (!rendera.modelLoader.hasModel({id: modelId})) {
+			if (!rendera.modelLoader.modelLoading({id: modelId})) {
+				console.log('Loading model', modelId);
+				const blob = this.GetEditorBlob(modelId);
+				if (!blob) {
+					console.error('[rendera-controller] model not found', modelId);
+					return false;
+				}
+				rendera.modelLoader.loadModel(modelId, blob);
+			}
+			return false;
+		}
+		this.modelLoaded = true;
+		return true;
+	}
+
 	OnCreate() {
+		this.modelId = String(this._inst.GetPropertyValue('model-id'));
+		console.log('[rendera-controller] onCreate model-id', this._inst.GetPropertyValue('model-id'));
+		console.log('[rendera-controller] instance', this._inst);
+		console.log('[rendera-controller] project', this._inst.GetProject());
+		console.log('[rendera-controller] project.GetModel', this._inst.GetProject().GetProjectFileByName('bug.glb'));
 	}
 	
 	OnPropertyChanged(id: string, value: EditorPropertyValueType) {
@@ -24,15 +82,20 @@ class RenderaControllerInstance extends SDK.IWorldInstanceBase {
 
 	Draw(iRenderer: SDK.Gfx.IWebGLRenderer, iDrawParams: SDK.Gfx.IDrawParams)
 	{
-		// console.log('Draw', iRenderer, iDrawParams);
+		debugger
 		const rendera = globalThis.mikalRenderaEditor;
-		if (rendera) {
-			if (!rendera.initialized && !rendera.initializing) {
-				rendera.initializing = true;
-				rendera.initialize();
-			}
-			return;
+		if (!this.IsRenderaInitialized(iRenderer)) return;
+		if (!this.IsModelLoaded(this.modelId)) return;
+		if (!this.modelCreated) {
+			this.model = rendera.instanceManager.createModel(this.modelId,null)
+			console.log('model created', this.model);
+			this.model.setScale(50,50,50);
+			this.modelCreated = true;
 		}
+
+		this.model.setPosition(this._inst.GetX(), this._inst.GetY(), this._inst.GetTotalZElevation());
+		rendera.Draw()
+		return;
 
 		const texture = this.GetTexture();
 		if (texture)
